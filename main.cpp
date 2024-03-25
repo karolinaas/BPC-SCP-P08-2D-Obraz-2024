e8a7daa061fd3171ac7295da0fe86c825f56114b
karolinaas <110686580+karolinaas@users.noreply.github.com>
2024-03-26 00:06:40 +0100

    Add boilerplate

---
+++ main.cpp
@@ -0,0 +1,125 @@
+#include <iostream>
+
+template<class T> class Obraz {
+	T** pole;
+	int width, length;
+
+public:
+	// Inicializaèní konstruktor
+	Obraz() {
+
+	}
+
+	// Kopírovací konstruktor
+	Obraz() {
+
+	}
+
+	// Konverzní konstruktor
+	Obraz() {
+
+	}
+
+	// Destruktor
+	~Obraz() {
+
+	}
+
+	// Binární operátory +, -, * (konvoluce), +=, -=
+	friend Obraz operator+(const Obraz& operand1, const Obraz& operand2);
+	friend Obraz operator-(const Obraz& operand1, const Obraz& operand2);
+	friend Obraz operator*(const Obraz& operand1, const Obraz& operand2);
+	friend Obraz operator+=(const Obraz& operand1, const Obraz& operand2);
+	friend Obraz operator-=(const Obraz& operand1, const Obraz& operand2);
+
+	// Unární operátor ~ pro výpoèet negativu
+	Obraz operator~() {
+		Obraz result();
+
+		return result;
+	}
+
+	// Operátory pro rovnost, nerovnost, pøiøazení
+	Obraz& operator==(const Obraz& operand) {
+
+	}
+	Obraz& operator!=(const Obraz& operand) {
+
+	}
+	Obraz& operator=(const Obraz& operand) {
+
+	}
+
+	// Operátor [] pro vracení/pøiøazení intenzity pixelu
+	T* operator[](int i) const {
+		return pole[i];
+	}
+
+	// Operátor << pro uložení do souboru
+	friend std::ostream& operator<<(std::ostream& out, Obraz& x);
+
+	// Funkce pro filtraci obrazu, prahování, detekci objektu
+	Obraz filter() {
+		Obraz result();
+
+		return result;
+	}
+	Obraz prahovani() {
+		Obraz result();
+
+		return result;
+	}
+	Obraz object_detection() {
+		Obraz result();
+
+		return result;
+	}
+};
+
+// Binární operátor +
+Obraz operator+(const Obraz& operand1, const Obraz& operand2) {
+	Obraz result();
+
+	return result;
+}
+// Binární operátor -
+Obraz operator-(const Obraz& operand1, const Obraz& operand2) {
+	Obraz result();
+
+	return result;
+}
+// Binární operátor * (konvoluce)
+Obraz operator*(const Obraz& operand1, const Obraz& operand2) {
+	Obraz result();
+
+	return result;
+}
+// Binární operátor +=
+Obraz operator+=(const Obraz& operand1, const Obraz& operand2) {
+	Obraz result();
+
+	return result;
+}
+// Binární operátor -=
+Obraz operator-=(const Obraz& operand1, const Obraz& operand2) {
+	Obraz result();
+
+	return result;
+}
+
+// Operátor << pro uložení do souboru
+std::ostream& operator<<(std::ostream& out, Obraz& x) {
+
+	return out;
+}
+
+class Pixel {
+
+};
+
+
+int main() {
+	std::cout << "Hello World" << std::endl;
+
+	return 0;
+}
