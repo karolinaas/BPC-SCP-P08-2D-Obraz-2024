ad40f38349b6f380627b717177df6aee5d2fb229
karolinaas <110686580+karolinaas@users.noreply.github.com>
2024-04-30 20:24:02 +0200

    Reading file and info header

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 #include <fstream>
 #include <cstdint>
+#include <bitset>
+
+#define FILE_HEADER_SIZE 14
+#define INFO_HEADER_SIZE 40
 
 template<class T> class Obraz {
 	T** pole;
@@ -123,14 +127,65 @@ int main() {
 	std::cout << "Hello World" << std::endl;
 
 	std::ifstream file;
-	char test[14];
+	uint8_t file_header[FILE_HEADER_SIZE]; // Buffer pro BMP hlavièku (prvních 14 bytù souboru)
+	uint8_t info_header[INFO_HEADER_SIZE];
 
 	file.open("bmp_test_input_bw.bmp", std::ios::binary | std::ios::in);
-	file.read((char*)&test, sizeof(test));
+	file.read((char*)&file_header, sizeof(file_header));
+	file.read((char*)&info_header, sizeof(info_header));
 
-	for (int i = 0; i < 14; i++) {
-		std::cout << std::hex << (unsigned int)test[i] << " ";
-	}
+	// Promìnné pro konkrétní data pøeètená z hlavièky souboru
+	uint8_t signature[2];
+	uint32_t file_size; // Velková velikost souboru v bytech
+	uint32_t data_offset; // Offset, neboli adresa v bajtech, kde se nachází pixelové data
+
+	uint32_t info_header_size;
+	uint32_t bm_width;
+	uint32_t bm_height;
+	uint16_t plane_count;
+	uint16_t bit_depth;
+	uint32_t compression;
+	uint32_t image_size;
+	uint32_t h_res;
+	uint32_t v_res;
+	uint32_t num_colors;
+	uint32_t num_important_colors;
+	
+	memcpy(&signature, &file_header[0], 2);
+	memcpy(&file_size, &file_header[2], 4);
+	memcpy(&data_offset, &file_header[10], 4);
+
+	memcpy(&info_header_size, &info_header[0], 4);
+	memcpy(&bm_width, &info_header[4], 4);
+	memcpy(&bm_height, &info_header[8], 4);
+	memcpy(&plane_count, &info_header[12], 2);
+	memcpy(&bit_depth, &info_header[14], 2);
+	memcpy(&compression, &info_header[16], 4);
+	memcpy(&image_size, &info_header[20], 4);
+	memcpy(&h_res, &info_header[24], 4);
+	memcpy(&v_res, &info_header[28], 4);
+	memcpy(&num_colors, &info_header[32], 4);
+	memcpy(&num_important_colors, &info_header[36], 4);
+
+	std::cout << std::endl << "FILE HEADER:" << std::endl;
+
+	std::cout << "File signature: " << signature[0] << signature[1] << std::endl;
+	std::cout << "File size (bytes): " << file_size << std::endl;
+	std::cout << "Data offset (bytes): " << data_offset << std::endl;
+
+	std::cout << std::endl << "INFO HEADER:" << std::endl;
+
+	std::cout << "Info header size (bytes): " << info_header_size << std::endl;
+	std::cout << "Bitmap width (pixels): " << bm_width << std::endl;
+	std::cout << "Bitmap height (pixels): " << bm_height << std::endl;
+	std::cout << "Number of planes: " << plane_count << std::endl;
+	std::cout << "Bit depth (bits): " << bit_depth << std::endl;
+	std::cout << "Compression type (0 = no compression): " << compression << std::endl;
+	std::cout << "(Compressed) image size (bytes): " << image_size << std::endl;
+	std::cout << "Horizontal resolution (pixels/meter): " << h_res << std::endl;
+	std::cout << "Vertical resolution (pixels/meter): " << v_res << std::endl;
+	std::cout << "Number of colors: " << num_colors << std::endl;
+	std::cout << "Number of important colors: " << num_important_colors << std::endl;
 
 	return 0;
 }
